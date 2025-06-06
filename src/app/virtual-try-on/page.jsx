2be831ec7a29dcf44472c5f3568c8cdc5b4ebd51
jeffzynoe3510 <ayooluwainioluwa2@gmail.com>
2025-06-06 08:53:21 -0700

    Update page.jsx

--- src/app/virtual-try-on/page.jsx
+++ src/app/virtual-try-on/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 
-import { useUpload } from "../utilities/runtime-helpers";
+import { useHandleStreamResponse } from "../../utilities/runtime-helpers";
 
 function MainComponent() {
   const { data: user } = useUser();
